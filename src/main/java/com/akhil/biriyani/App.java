e4e67173dfdb51773c230c24d3ef5290a6a19833
Bharath <bharathpantala@gmail.com>
2018-06-03 09:38:30 -0700

    1.0

---
+++ src/main/java/com/akhil/biriyani/App.java
@@ -0,0 +1,13 @@
+package com.akhil.biriyani;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
